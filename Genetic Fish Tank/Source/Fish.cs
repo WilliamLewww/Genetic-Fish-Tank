9e1c915cea67803d643c35d29fba9f59fe4c386c
WilliamLewww <WilliamLew32@gmail.com>
2015-11-09 22:00:49 -0800

    added seperate proximity class

--- Genetic Fish Tank/Source/Fish.cs
+++ Genetic Fish Tank/Source/Fish.cs
@@ -8,14 +8,12 @@ namespace Genetic_Fish_Tank.Source
     class Fish
     {
         NeuralNetwork brain;
-        ProximitySensor proximitySensor;
+        ProximitySensor[] proximitySensor = new ProximitySensor[2];
 
         Texture2D texture;
         Vector2 position;
         
         Vector2 origin;
-
-        Food closestFood;
 
         static Random random = new Random();
 
@@ -50,18 +48,17 @@ namespace Genetic_Fish_Tank.Source
             origin = new Vector2(texture.Width / 2, texture.Height / 2);
 
             rotation = 90;
-
-            proximitySensor = new ProximitySensor();
+            proximitySensor[0] = new ProximitySensor((int)(position.X), (int)(position.Y), rotation, -5);
+            proximitySensor[1] = new ProximitySensor((int)(position.X), (int)(position.Y), rotation, 5);
         }
 
         public void Update(GameTime gameTime)
         {
-            closestFood = proximitySensor.GetClosestFood(FishRectangle, Tank.foodList);
             int[] input = new int[2];
-            if (closestFood.FoodRectangle.X + closestFood.FoodRectangle.Width < rectangle.X) input[0] = 1;
-            else input[0] = 0;
-            if (closestFood.FoodRectangle.X > rectangle.X + rectangle.Width) input[1] = 1;
-            else input[1] = 0;
+
+            foreach (ProximitySensor sensor in proximitySensor)
+                sensor.Update(gameTime, (int)(position.X), (int)(position.Y), rotation);
+
             brain.SendInput(input);
 
             rectangle = new Rectangle((int)(position.X), (int)(position.Y), texture.Width, texture.Height);
@@ -70,6 +67,9 @@ namespace Genetic_Fish_Tank.Source
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(texture, position, null, Color.White, MathHelper.ToRadians(rotation), origin, 1f, SpriteEffects.None, 0);
+
+            foreach (ProximitySensor sensor in proximitySensor)
+                sensor.Draw(spriteBatch);
         }
     }
 
@@ -81,11 +81,44 @@ namespace Genetic_Fish_Tank.Source
         int rotation;
         Vector2 origin;
 
+        public Vector2 theoreticalPosition;
+
         public Sensor(int x, int y, int z)
         {
             texture = Fish.Content.Load<Texture2D>("Sprites/fishsensor.png");
             position = new Vector2(x, y);
             origin = new Vector2(texture.Width / 2, texture.Height / 2 + 49);
+            rotation = z;
+        }
+
+        public void Update(GameTime gameTime, int x, int y, int z)
+        {
+            position = new Vector2(x, y);
+            rotation = z;
+
+            theoreticalPosition.X = (float)(position.X * Math.Cos(rotation)) - (float)(position.Y * Math.Sin(rotation));
+            theoreticalPosition.Y = (float)(position.X * Math.Sin(rotation)) + (float)(position.Y * Math.Cos(rotation));
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, MathHelper.ToRadians(rotation), origin, 1f, SpriteEffects.None, 0);
+        }
+    }
+
+    class ProximitySensor
+    {
+        Texture2D texture;
+        Vector2 position;
+
+        int rotation;
+        Vector2 origin;
+
+        public ProximitySensor(int x, int y, int z, int offset)
+        {
+            texture = Fish.Content.Load<Texture2D>("Sprites/proximitysensor.png");
+            position = new Vector2(x, y);
+            origin = new Vector2(texture.Width / 2 + offset, texture.Height / 2 + 20);
             rotation = z;
         }
 
@@ -101,23 +134,31 @@ namespace Genetic_Fish_Tank.Source
         }
     }
 
-    class ProximitySensor
+    class UniversalSensorFunctions
     {
-        public Food GetClosestFood(Rectangle rectangle, Food[] foodList)
+        public Food GetClosestFood(Vector2 position, Food[] foodList)
         {
             int closestID = 0;
-            float closestValue = Math.Abs(rectangle.X - foodList[0].FoodRectangle.X) + Math.Abs(rectangle.Y - foodList[0].FoodRectangle.Y);
+            float closestValue = Math.Abs(position.X - foodList[0].FoodRectangle.X) + Math.Abs(position.Y - foodList[0].FoodRectangle.Y);
 
             for (int x = 0; x < foodList.Length; x++)
             {
-                if (Math.Abs(rectangle.X - foodList[x].FoodRectangle.X) + Math.Abs(rectangle.Y - foodList[x].FoodRectangle.Y) < closestValue)
+                if (Math.Abs(position.X - foodList[x].FoodRectangle.X) + Math.Abs(position.Y - foodList[x].FoodRectangle.Y) < closestValue)
                 {
                     closestID = x;
-                    closestValue = Math.Abs(rectangle.X - foodList[x].FoodRectangle.X) + Math.Abs(rectangle.Y - foodList[x].FoodRectangle.Y);
+                    closestValue = Math.Abs(position.X - foodList[x].FoodRectangle.X) + Math.Abs(position.Y - foodList[x].FoodRectangle.Y);
                 }
             }
 
             return foodList[closestID];
+        }
+
+        public bool CompareClosest(Vector2 position, Food foodLeft, Food foodRight)
+        {
+            if (Math.Abs(position.X - foodLeft.FoodRectangle.X) + Math.Abs(position.Y - foodLeft.FoodRectangle.Y) < Math.Abs(position.X - foodRight.FoodRectangle.X) + Math.Abs(position.Y - foodRight.FoodRectangle.Y))
+                return true;
+
+            return false;
         }
     }
 }
